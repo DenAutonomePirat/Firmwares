36b212703134ab6c4132ba8be00737ba90d04971
DenAutonomePirat <DenAutonomePirat@users.noreply.github.com>
2015-09-27 21:21:33 +0200

    added description

--- libraries/EncoderLib/EncoderLib.cpp
+++ libraries/EncoderLib/EncoderLib.cpp
@@ -1,7 +1,5 @@
 /*
-  Morse.cpp - Library for flashing Morse code.
-  Created by David A. Mellis, November 2, 2007.
-  Released into the public domain.
+  EncoderLib.cpp Library for sensor AEAT 6010
 */
 
 #include "Arduino.h"
