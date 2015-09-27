44f3d82087a1a633edd39a8040baf55fb3760a7b
DenAutonomePirat <DenAutonomePirat@users.noreply.github.com>
2015-09-27 21:20:13 +0200

    added description

--- libraries/EncoderLib/EncoderLib.h
+++ libraries/EncoderLib/EncoderLib.h
@@ -1,7 +1,5 @@
 /*
-  Morse.h - Library for flashing Morse code.
-  Created by David A. Mellis, November 2, 2007.
-  Released into the public domain.
+  EncoderLib.h Library for sensor AEAT 6010
 */
 #ifndef EncoderLib_h
 #define EncoderLib_h
